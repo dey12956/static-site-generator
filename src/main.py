e27596e3057dbea4884b67387ecf696ebd3fc5cf
dey12956 <dey12956@gmail.com>
2025-04-26 23:09:21 +0800

    minor bug fixed

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from generate_page import generate_pages_recursive
 import sys
 
 def main():
-    basepath = sys.argv[1]
-    if not sys.argv[1]:
+    basepath = sys.argv[0]
+    if not sys.argv[0]:
         basepath = "/"
     cp_dirtree("./static", "./docs")
     generate_pages_recursive(basepath, "template.html", "docs")
